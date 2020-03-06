20e78983f6ccf87cab04637476944dae449c79bf
Ysilence <56149087+Ysilence@users.noreply.github.com>
2020-03-06 11:44:56 +0800

    UI

--- app/src/main/java/com/example/db/MainActivity.java
+++ app/src/main/java/com/example/db/MainActivity.java
@@ -9,7 +9,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView t1;
-    private EditText t2;
+    private EditText tname;
+    private EditText temail;
+    private EditText tnumber;
     private Button btwrite;
     private Button btread;
     private Button btupdate;
@@ -20,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        t1=(TextView)findViewById(R.id.txtshow);
+
+        tname=(EditText)findViewById(R.id.etname);
+        temail=(EditText)findViewById(R.id.etemail);
+        tnumber=(EditText)findViewById(R.id.etnumber);
+
+        btwrite=(Button)findViewById(R.id.Bwrite);
+        btread=(Button)findViewById(R.id.Bread);
+        btupdate=(Button)findViewById(R.id.Bupdate);
+        btremove=(Button)findViewById(R.id.Bremove);
+
+
     }
 }
